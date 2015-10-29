d8fc4610d86985313e9e03333fbcffe51c302817
Phiromporn <naphaphorn_123@live.com>
2015-10-29 11:26:43 +0700

    no message

--- SCM_PROJECT/src/com/scm/Salespeople.java
+++ SCM_PROJECT/src/com/scm/Salespeople.java
@@ -30,20 +30,16 @@ public class Salespeople {
 		}
 		public double wages() {
 		
-			if(circulation>0 && circulation <= 25000 ){
+			if(circulation>0 && circulation <= 50000 ){
 				this.wage = circulation*1.01;
 				
-			}else if(circulation>25000 &&circulation <= 50000 ){
+			}else if(circulation>50000 &&circulation <= 100000 ){
 				this.wage = circulation*1.02;
 				
-			}else if(circulation>50000 ){
+			}else if(circulation>100000 ){
 				this.wage = circulation*1.03;
 				
 			}
-			if(this.wage < 18000){
-				this.wage = 18000;
-			}
-			
 			
 			return wage;
 		}
