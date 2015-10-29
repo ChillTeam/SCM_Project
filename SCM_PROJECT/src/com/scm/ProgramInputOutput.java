4770f63a3c7c175abace743bb9f3235178518294
Pratya <zee_pratya@hotmail.com>
2015-10-29 10:50:33 +0700

    เพิ่มการรับค่าจากหน้าจอและแสดงผลออกมา

---
+++ SCM_PROJECT/src/com/scm/ProgramInputOutput.java
@@ -0,0 +1,23 @@
+package com.scm;
+import java.util.*;
+
+public class ProgramInputOutput {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		String name="";
+		System.out.print("Input name : ");
+		name = input.nextLine();
+		double salary;
+		System.out.print("Input salary : ");
+		salary = input.nextDouble();
+		double circulation;
+		System.out.print("Input circulation : ");
+		circulation = input.nextDouble();
+		Salespeople emp = new Salespeople(name, salary, circulation) ;
+		System.out.println("Name  : "+emp.getName());
+		System.out.println("Salary  : "+emp.getSalary());
+		System.out.println("Circulation  : "+emp.getCirculation());
+		System.out.println("Wage : "+emp.wages());
+	}
+}
